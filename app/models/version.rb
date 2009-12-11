6f81b96da5486df9ebf298f421f454e3848d0aad
zhangzhe <gary20071226@gmail.com>
2009-12-12 01:17:56 +0800

    add

--- app/models/version.rb
+++
@@ -1,3 +0,0 @@
-class Version < ActiveRecord::Base
-  belongs_to :blog
-end
--- db/migrate/20091207100244_add_description_to_upload_file.rb
+++
@@ -1,8 +0,0 @@
-class AddDescriptionToUploadFile < ActiveRecord::Migration
-  def self.up
-    add_column :upload_files, :description, :text
-  end
-
-  def self.down
-  end
-end
