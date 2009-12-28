580444ac1305fc1e5149cfe153958c67a322320c
GARY <gary@gary-laptop.(none)>
2009-12-28 23:13:33 +0800

    change paginate

--- app/controllers/blog_groups_controller.rb
+++ app/controllers/blog_groups_controller.rb
@@ -3,10 +3,6 @@ class BlogGroupsController < ApplicationController
     @blog_groups = BlogGroup.paginate(:all, :page => params[:page], :per_page => 5, :order => "created_at DESC")
   end
 
-  def list
-    @blog_groups = BlogGroup.all
-  end
-  
   def destroy
     BlogGroup.find(params[:id]).destroy
     redirect_to :back
