8a6f1e1186e4d5dd3c55dd5c1fabf06438b2bb48
GARY <gary@gary-laptop.(none)>
2010-01-02 11:35:04 +0800

    improve comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.create(params[:comment].merge(:blog_id => params[:blog_id]))
+    unless params[:comment][:name].blank? or params[:comment][:content].blank?
+      @comment = Comment.create(params[:comment].merge(:blog_id => params[:blog_id]))
+    end
     redirect_to :back
   end
 end
