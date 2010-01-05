5cd61a26c2da1ff126b7a5bf1d0676b4100481ce
zhangzhe <gary20071226@gmail.com>
2010-01-05 16:06:56 +0800

    improve rss

---
+++ config/schedule.rb
@@ -0,0 +1,15 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+
+#remember to create this log: /home/app/applications/scenario/current/log/whenever.log
+
+# set :output, "/home/gary/idapted/zhangzhe/log/whenever.log"
+# every 1.minutes do
+#   runner "puts ('ye')"
+#   command "date"
+# end
