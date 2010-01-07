2f9a096b5462bca7dbd4e9f15a499423e352413d
zhangzhe <gary20071226@gmail.com>
2010-01-07 17:49:44 +0800

    improve css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,7 @@ module ApplicationHelper
     @blogs = English.find(:all, :limit => 10, :order => "created_at DESC")
     result = "<ul>"
     @blogs.each do |blog|
-      result << "<li>#{link_to(truncate(blog.title, :length => 27), blog_path(blog), :title => blog.title)}</li>"
+      result << "<li>#{link_to(truncate(blog.title, :length => 30), blog_path(blog), :title => blog.title)}</li>"
     end
     result << "</ul>"
   end
