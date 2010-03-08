1272c6a674ec0953af49c5a3f132e037ac11c222
zhangzhe <gary20071226@gmail.com>
2010-03-08 18:36:04 +0800

    add Chinese_in_url_to_PinYin and attachment_fu_for_chinese_char

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,17 +2,7 @@ class Blog < ActiveRecord::Base
   acts_as_taggable_on :tags
   belongs_to :blog_group
   has_many :comments
-  require "pinyin"
-  after_save do |blog|
-    Blog.transaction do 
-      new_u_title = PinYin.instance.to_permlink(blog.title) if blog.title
-      blog.update_attributes(:u_title => new_u_title) unless new_u_title == blog.u_title
-    end
-  end
-  
-  def to_param
-    return "#{id}-#{u_title}"
-  end
+  pinyin_title :title
   
   def content_blank?
     title.blank? or content.blank?
